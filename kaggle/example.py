99d12223bb3c78a0fef584bc932da653fce79c3e
Zerhigh <samuel@plix.at>
2022-12-18 23:06:03 +0100

    kaggle_semantic project

--- kaggle/example.py
+++ kaggle/example.py
@@ -4,7 +4,13 @@ import numpy as np
 import cv2
 import os
 from keras_segmentation.models.unet import vgg_unet
+import keras_segmentation
+from keras.models import *
 import time
+import tensorflow as tf
+
+from types import MethodType
+
 
 original_image = "./input/semantic_drone_dataset/training_set/images/001.jpg"
 label_image_semantic_RAW = "./input/semantic_drone_dataset/training_set/gt/semantic/label_images/001.png"
@@ -41,29 +47,35 @@ print(len(uniques))
 """
 
 ### Reassign all images ###
-"""
+
+# reassign_hash = {0: 0, 1: 128, 2: 2, 3: 130, 4: 102, 5: 70, 6: 9, 7: 107, 8: 112, 9: 48, 10: 51, 11: 119, 12: 254, 13: 153, 14: 28, 15: 190, 16: 255}
+# reassign_hash = {0: 0, 1: 128, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0, 10: 0, 11: 0, 12: 0, 13: 0, 14: 0, 15: 0, 16: 0}
 reassign_hash = {0: 0, 1: 128, 2: 2, 3: 130, 4: 102, 5: 70, 6: 9, 7: 107, 8: 112, 9: 48, 10: 51, 11: 119, 12: 254, 13: 153, 14: 28, 15: 190, 16: 255}
 
 for arr in os.listdir(label_image_semantic):
     blab = np.asarray(Image.open(label_image_semantic + arr))[:, :, 0]
     image = blab.copy()
-    for k, v in reassign_hash.items():
-        image[image == v] = k
+    image[image == 128] = 1
+    image[image != 128] = 0
+    #for k, v in reassign_hash.items():
+    #    image[image == v] = k
     im = Image.fromarray(image)
     im.save(f"./input/semantic_drone_dataset/training_set/gt/semantic/rehashed/{arr}")
     print(f"saved {arr}")
-"""
 
-### Train Model and display one result ###
+
+### Train Model ###
+
 # change epoch numbers
-kaggle_commit = True
+kaggle_commit = False
 epochs = 20
 if kaggle_commit:
     epochs = 5
+epochs = 20
 
 # Aerial Semantic Segmentation Drone Dataset tree, gras, other vegetation, dirt, gravel, rocks, water, paved area, pool, person, dog, car, bicycle, roof, wall, fence, fence-pole, window, door, obstacle
 
-n_classes = 22
+n_classes = 2 #22
 model = vgg_unet(n_classes=n_classes ,  input_height=416, input_width=608)
 
 # train model vgg_unet (?)
@@ -72,20 +84,34 @@ model.train(
     train_annotations = "./input/semantic_drone_dataset/training_set/gt/semantic/rehashed/",
     checkpoints_path = "vgg_unet", epochs=epochs)
 
-model.save('try1812_example')
+model_name = 'model_2class_1812_20e'
+model.save(model_name)
 
+
+### Load Model ###
+"""
+n_classes = 22
+reconstructed_model = load_model("model_1812_example")
+reconstructed_model.predict_segmentation = MethodType(keras_segmentation.predict.predict, reconstructed_model)
+reconstructed_model.input_width = 608
+reconstructed_model.input_height= 416
+reconstructed_model.output_width = 208
+reconstructed_model.output_height = 304
+reconstructed_model.n_classes=n_classes
+"""
 
 ### Predict result ###
-"""
+
 start = time.time()
 
-input_image = "./input/semantic_drone_dataset/training_set/images/001.jpg"
-out = model.predict_segmentation(
-    inp=input_image,
-    out_fname="out.png"
-)
+for arr in os.listdir(label_image_semantic)[:15]:
+    input_image = f"./input/semantic_drone_dataset/training_set/images/{arr}"
+    out = model.predict_segmentation(
+        inp=input_image,
+        out_fname=f"{arr.split('.')[0]}.png"
+    )
 
-fig, axs = plt.subplots(1, 3, figsize=(20, 20), constrained_layout=True)
+fig, axs = plt.subplots(1, 2, figsize=(8, 8), constrained_layout=True)
 
 img_orig = Image.open(input_image)
 axs[0].imshow(img_orig)
@@ -101,6 +127,33 @@ axs[2].imshow( Image.open(validation_image))
 axs[2].set_title('true label image-001.png')
 axs[2].grid(False)
 
-done = time.time()
-elapsed = done - start
+plt.show()
+
+
+### Use Pretrained model ###
+"""from .models.all_models import model_from_name
+def model_from_checkpoint_path(model_config, latest_weights):
+
+    model = model_from_name[model_config['model_class']](
+        model_config['n_classes'], input_height=model_config['input_height'],
+        input_width=model_config['input_width'])
+    model.load_weights(latest_weights)
+    return model
+
+def pspnet_101_voc12():
+
+    model_config = {
+        "input_height": 473,
+        "input_width": 473,
+        "n_classes": 21,
+        "model_class": "pspnet_101",
+    }
+
+    model_url = "https://www.dropbox.com/s/" \
+                "uvqj2cjo4b9c5wg/pspnet101_voc2012.h5?dl=1"
+    latest_weights = tf.keras.utils.get_file("pspnet101_voc2012.h5", model_url)
+
+    return model_from_checkpoint_path(model_config, latest_weights)
+
+model = pspnet_101_voc12()
 """
---
+++ kaggle/test.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+arr = np.ones((4, 4))
+print(arr)
+
+arr[arr==1] =4
+print(arr)
